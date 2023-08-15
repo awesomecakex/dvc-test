7fdd223df32f221a16a3d33e4a3370763238d064
ido reshef <idoreshef10@gmail.com>
2023-08-15 09:20:53 +0300

    delete all

--- testing.js
+++ testing.js
@@ -15,6 +15,10 @@ function getDvcDiff() {
     lines.forEach(line => {
         const trimmedLine = line.trim();
 
+        if (trimmedLine.startsWith('files summary')) {
+            return; // Skip the "files summary" line
+        }
+
         if (trimmedLine === 'Added:') {
             currentCategory = 'added';
         } else if (trimmedLine === 'Modified:') {
@@ -43,7 +47,63 @@ function getDvcDiff() {
     };
 }
 
-const {added, modified, deleted} = getDvcDiff();
-console.log(added);
-console.log(modified);
-console.log(deleted);
+
+function executeCommand(command) {
+    execSync(command, { encoding: 'utf8', stdio: 'inherit' });
+}
+
+
+function handleUntrackedFiles() {
+    // Get the list of untracked files from Git
+    let result = execSync('git ls-files --others --exclude-standard').toString();
+
+    // Split the result into an array of filenames
+    let untrackedFiles = result.trim().split('\n');
+
+    // Filter files if necessary, e.g., if you only want to add .csv files to DVC
+    let dataFiles = untrackedFiles.filter(file => file.endsWith('.csv'));
+
+    // Run `dvc add` on these files
+    dataFiles.forEach(file => {
+        execSync(`dvc add ${file}`);
+        execSync(`git add ${file}.dvc`);
+    });
+}
+
+function handleAddedFiles(files) {
+    files.forEach(file => {
+        executeCommand(`dvc add ${file}`);
+        executeCommand(`git add ${file}.dvc`);
+    });
+}
+
+function handleModifiedFiles(files) {
+    // The process of handling modified files in DVC is similar to adding them
+    handleAddedFiles(files);
+}
+
+function handleDeletedFiles(files) {
+    files.forEach(file => {
+        executeCommand(`git rm ${file}.dvc`);
+        // No need to remove the actual data file since you've already deleted it
+        // If you haven't deleted it yet, you can also add: executeCommand(`rm ${file}`);
+    });
+}
+
+function commitAndPushChanges(message) {
+    executeCommand(`git commit -m "${message}"`);
+    executeCommand('dvc push');
+    executeCommand('git push');
+}
+
+// Use the functions based on the diff result:
+const diffResult = getDvcDiff();  // Assuming you have the getDvcDiff function from previous examples
+
+handleDeletedFiles(diffResult.deleted);
+handleUntrackedFiles();
+handleAddedFiles(diffResult.added);
+handleModifiedFiles(diffResult.modified);
+
+
+// After all files are processed, commit and push the changes
+commitAndPushChanges("Updated data files");
